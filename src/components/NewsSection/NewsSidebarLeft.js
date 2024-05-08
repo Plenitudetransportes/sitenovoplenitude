d186d5646ed3c916e06253cc306e5a8623ebfe82
tiobetobr <158787190+tiobetobr@users.noreply.github.com>
2024-05-07 22:54:15 -0300

    primeiro commit

--- src/components/NewsSection/NewsSidebarLeft.js
+++ src/components/NewsSection/NewsSidebarLeft.js
@@ -1,17 +1,6 @@
 import React from "react";
 import BlogPagination from "./BlogPagination";
 
-const NewsSidebarLeft = ({ newses = [] }) => {
-  return (
-    <div className="news-sidebar__left">
-      <div className="news-sidebar__content">
-        {newses.map((news) => (
-          <NewsSidebarSingle news={news} key={news.id} />
-        ))}
-        <BlogPagination />
-      </div>
-    </div>
-  );
-};
+
 
 
